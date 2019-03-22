6066c4748e2d49de0f67a0fc8ce0cf33fc5871cd
James Dolphin <j.dolphin2013@gmail.com>
2019-03-22 12:02:11 +1100

    error catch/configurable welcome message

--- discordWelcome.js
+++ discordWelcome.js
@@ -3,8 +3,10 @@ const bot = new Discord.Client ();
 const env = require('dotenv').config();
 
 bot.on('guildMemberAdd', member => {
-   member.send("Hello, thanks for joining our discord server...");
+   member.send(process.env.WELCOMEMESSAGE);
    bot.channels.get(process.env.DCLOGCHANNEL).send(`${member}` + ' has joined the server and has been sent a PM.');
 });
+
+bot.on('error', console.error);
 
 bot.login(process.env.DCTOKEN);
