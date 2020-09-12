9241fb84250bb556238dc0a25c9d81668ab4d05a
JamesDolphin <james@4zerotechnologies.com>
2020-09-12 19:55:01 +1000

    added commands

--- discordWelcome.js
+++ discordWelcome.js
@@ -1,12 +1,45 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client ();
+const bot = new Discord.Client();
 const env = require('dotenv').config();
 
+let messageLink = '';
+let channelId = '';
+let messageId = '';
+
 bot.on('guildMemberAdd', member => {
-   member.send(process.env.WELCOMEMESSAGE);
-   bot.channels.get(process.env.DCLOGCHANNEL).send(`${member}` + ' has joined the server and has been sent a PM.');
+   if (messageLink !== '') {
+      member.send(welcomeMessage(member));
+   }
+   //bot.channels.get(process.env.DCLOGCHANNEL).send(`${member}` + ' has joined the server and has been sent a PM.');
+});
+
+bot.on('message', msg => {
+   //IF msg.Author is ADMIN OR OWNER CONTINUE BELOW
+
+   if (msg.content.includes('!message')) {
+      messageLink = msg.content.replace('!message https://discordapp.com/channels/', '');
+
+      let args = messageLink.split("/");
+
+      if (args.length != 3) {
+         msg.reply("Incorrect message link");
+      } else {
+         channelId = args[1];
+         messageId = args[2];
+         msg.react('✅');
+      }
+   }
 });
 
 bot.on('error', console.error);
 
 bot.login(process.env.DCTOKEN);
+
+//Sends message to user who joined discord
+function welcomeMessage(member) {
+   let channel = bot.channels.find(x => x.id == channelId);
+
+   channel.fetchMessage(messageId)
+      .then(message => member.send(message.content))
+      .catch(console.error);
+}
